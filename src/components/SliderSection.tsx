73755421fbb6d9bad3ba04feb5641c91b0f47d51
Fahridanaa <fahridanaaa@gmail.com>
2024-10-14 13:46:07 +0700

    feat: add popular comic update slider section

--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -1,10 +1,13 @@
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 const SliderSection = ({ children }) => (
-	<ScrollArea>
-		{children}
-		<ScrollBar orientation="horizontal" />
-	</ScrollArea>
+	<div>
+		<h3 className="font-medium text-xl mb-1">Popular Comic Update</h3>
+		<ScrollArea className="py-2">
+			<div className="flex w-max space-x-4 p-2 gap-4">{children}</div>
+			<ScrollBar orientation="horizontal" />
+		</ScrollArea>
+	</div>
 );
 
 export default SliderSection;
