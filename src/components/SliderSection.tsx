de6347157932f28e43dd4dae2acfafd04ba9c4de
Fahridanaa <fahridanaaa@gmail.com>
2024-10-14 14:06:07 +0700

    feat: add latest update section

--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -2,7 +2,7 @@ import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 const SliderSection = ({ children }) => (
 	<div>
-		<h3 className="font-medium text-xl mb-1">Popular Comic Update</h3>
+		<h2 className="font-medium text-xl mb-1">Popular Comic Update</h2>
 		<ScrollArea className="py-2">
 			<div className="flex w-max space-x-4 p-2 gap-4">{children}</div>
 			<ScrollBar orientation="horizontal" />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 	];
 
 	return (
-		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
 			<NavigationBar logo="" items={navItems} />
 		</header>
 	);
