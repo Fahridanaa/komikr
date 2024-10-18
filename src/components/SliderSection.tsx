e27475e25d13522e447bdc15c72de9f99b0f6e63
Fahridanaa <fahridanaaa@gmail.com>
2024-10-19 01:23:15 +0700

    chore: change SliderSection to astro component

--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -1,8 +1,14 @@
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
-const SliderSection = ({ children }: { children: React.ReactNode }) => (
+const SliderSection = ({
+	title,
+	children,
+}: {
+	title: string;
+	children: React.ReactNode;
+}) => (
 	<div>
-		<h2 className="font-medium text-xl mb-1">Popular Comic Update</h2>
+		<h2 className="font-medium text-xl mb-1">{title}</h2>
 		<ScrollArea className="py-2">
 			<div className="flex w-max space-x-4 p-2 gap-4">{children}</div>
 			<ScrollBar orientation="horizontal" />
