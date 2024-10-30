8b5a03734e516f8e26ceae7f8324887f3e937c54
Fahridanaa <fahridanaaa@gmail.com>
2024-10-30 23:22:34 +0700

    style: add padding in the comic popular aside

--- src/components/ComicOverviewList.tsx
+++ src/components/ComicOverviewList.tsx
@@ -9,8 +9,11 @@ const ComicOverviewList: React.FC<ComicOverviewListProps> = ({ comics }) => {
 	return (
 		<div className="relative flex flex-col justify-center p-2">
 			{comics.map((comic, index) => (
-				<div key={index} className="relative flex gap-2 border-b-2">
-					<div className="absolute right-0">
+				<div
+					key={index}
+					className="relative flex gap-2 border-b-2 pb-2 mb-2"
+				>
+					<div className="absolute right-0 -top-2">
 						<span className="rounded-b-xl bg-blue-400 px-2 py-1 text-white font-bold">
 							{index + 1}
 						</span>
@@ -27,7 +30,7 @@ const ComicOverviewList: React.FC<ComicOverviewListProps> = ({ comics }) => {
 						</figure>
 					</div>
 					<div className="flex-1">
-						<h3 className="font-semibold text-lg">{comic.title}</h3>
+						<h3 className="font-semibold text-md">{comic.title}</h3>
 						<p className="text-sm">
 							<span className="text-muted-foreground">
 								Genres:
